52d6e0fb5e9067e27e0c35e9214c1316e381cd34
Pankaj Singh <pankaj.singh@dnata.com>
2021-09-23 17:14:46 +0400

    -- responsive code

--- app/molecule/flight-schedule-row.js
+++ app/molecule/flight-schedule-row.js
@@ -18,11 +18,15 @@ import './flight-schedule-row.scss';
 const FlightScheduleTableRow = (props) => {
     return (
         <ul className="schedule-row">
-            <li>{props.flight}</li>
-            <li>{props.from}</li>
-            <li>{props.to}</li>
-            <li>{props.dtime}</li>
-            <li>{props.atime}</li>
+            <li><span className="color-highlight">{props.dtime}</span> ({props.from})</li>
+            <li><span className="color-highlight">{props.atime}</span> ({props.to})</li>
+            <li><span className="color-highlight">{props.flight}</span></li>
+            <li>3h 30m</li>
+           
+            
+
+            {/* <li><ul className="schedule-row"><li>{props.flight}</li><li>{props.from}</li><li>{props.to}</li></ul></li>
+            <li><ul className="schedule-row"><li>{props.dtime}</li><li>{props.atime}</li></ul></li> */}
         </ul>
     );
 }
--- app/organism/flight-schedule-table.js
+++ app/organism/flight-schedule-table.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import FlightScheduleTableRow from '../molecule/flight-schedule-row';
+import './flight-schedule-table.scss'
 import mock from '../mock/flight-schedule.json';
 //TODO: Mock Below api call with jest
 
@@ -10,13 +11,19 @@ class FlightScheduleTable extends Component {
       <div className="fs-table">
         <div className="fs-body">
           Flight Schedule Table
+          <ul className="schedule-header">
+            <li className="depart">Depart</li>
+            <li className="arrive">Arrive</li>
+            <li className="flight">Flight</li>
+            <li className="duration">Total Duration</li>
+        </ul>
           {mock.map((flight) => (
               <FlightScheduleTableRow
                 flight={flight.flight}
                 from={flight.from}
                 to={flight.to}
-                depart-time={flight.dtime}
-                arrival-time={flight.atime}
+                dtime={flight.dtime}
+                atime={flight.atime}
               />
             ))}
         </div>
